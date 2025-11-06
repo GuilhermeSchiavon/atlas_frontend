0b18ce9156eaf89bc9047dabb99351139b39f8bc
Guilherme Unimagem <guilherme@schiavon.dev>
2025-11-06 17:47:49 -0300

    Ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [{
+    name: '3300_ATLAS_DERMATORIGICO_GM_FRONT',
+    script: 'npm',
+    args: 'start',
+    cwd: '/var/www/atlas/fronten',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3300
+    }
+  }]
+}
