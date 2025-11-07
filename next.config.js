28a65e610fbf79d4a931ed13a0bfd3b58c1fa3a5
Guilherme Unimagem <guilherme@schiavon.dev>
2025-11-07 10:39:19 -0300

    Img - Domains - Config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'api.atlasurodermato.com.br'],
     remotePatterns: [
       {
         protocol: 'http',
         hostname: 'localhost',
         port: '1000',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.atlasurodermato.com.br',
         pathname: '/uploads/**',
       },
     ],
